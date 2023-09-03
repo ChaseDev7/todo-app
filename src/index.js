60cc4be0e2d55b8c246efee72a255f0580ad773a
ChaseDev7 <chasewebdev101@gmail.com>
2023-09-03 14:40:05 -0700

    Create files, run test

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+const container = document.createElement("div");
+container.classList.add("container");
+container.textContent = "This is a test";
+
+document.body.appendChild(container);
